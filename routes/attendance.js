c0d74ff554200d7d723ce0a7ea8b16fd02c53b85
BilalAhmed25 <bilalahmed2520@gmail.com>
2025-11-26 04:59:31 +0500

    Bug fix in showing single user attendance api.

--- routes/attendance.js
+++ routes/attendance.js
@@ -173,8 +173,8 @@ router.get('/day', async (req, res) => {
 
 // -------------------- /user API --------------------
 router.get('/user', async (req, res) => {
-    const { employeeId, startDate, endDate } = req.query;
-    if (!employeeId || !startDate || !endDate) return res.status(400).json({ error: 'Missing parameters' });
+    const { user, startDate, endDate } = req.query;
+    if (!user || !startDate || !endDate) return res.status(400).json({ error: 'Missing parameters' });
 
     try {
         const [users] = await con.execute(`
@@ -184,8 +184,8 @@ router.get('/user', async (req, res) => {
             LEFT JOIN Departments d ON u.DepartmentID = d.ID
             LEFT JOIN Designations des ON u.DesignationID = des.ID
             WHERE u.ID = ? AND u.Status='Active' AND d.ID != 5
-        `, [employeeId]);
-
+        `, [user]);
+        console.log(users);
         if (!users.length) return res.status(404).json({ error: 'User not found' });
         const user = users[0];
 
@@ -233,7 +233,6 @@ router.get('/user', async (req, res) => {
         }
 
         res.json(results);
-
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Internal server error' });
